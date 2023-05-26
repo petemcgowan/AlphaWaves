4776df66bdef4cd038f25cca35a9392e58831a8c
Pete <peteburquette@gmail.com>
2023-05-26 21:49:36 +0100

    Some basics

---
+++ app/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { Text } from 'react-native'
+
+import SplashScreen from 'react-native-splash-screen'
+import CentralNavigation from './components/CentralNavigation'
+
+const App = () => {
+  // Ionicons.loadFont()
+
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
+  return <CentralNavigation />
+}
+
+export default App
