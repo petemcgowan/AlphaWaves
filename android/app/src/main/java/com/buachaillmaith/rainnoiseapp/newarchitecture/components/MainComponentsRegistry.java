5edeff9aff4c9dae44a6ad57187481e693e8e576
Pete <peteburquette@gmail.com>
2023-06-06 22:01:29 +0100

    restructuring

--- android/app/src/main/java/com/buachaillmaith/rainnoiseapp/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/buachaillmaith/rainnoiseapp/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.rainnoiseapp.newarchitecture.components;
+package com.buachaillmaith.rainnoiseapp.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
--- android/app/src/main/java/com/buachaillmaith/rainnoiseapp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/buachaillmaith/rainnoiseapp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.rainnoiseapp.newarchitecture.modules;
+package com.buachaillmaith.rainnoiseapp.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
