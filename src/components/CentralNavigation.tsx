ed185b79f5ddbc41ce5577ad9d5af53be6835a9e
Pete McGowan <peteburquette@gmail.com>
2023-11-24 19:45:26 +0400

    refactoring redux and components

---
+++ src/components/CentralNavigation.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import RainSlider from '../screens/RainSlider';
+import OnboardingDeck from '../screens/OnboardingDeck';
+import {useSelector} from 'react-redux';
+import {actionCreators} from '../redux/index';
+import {useDispatch} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {State} from '../redux/index';
+
+const AppStack = createStackNavigator();
+
+export default function CentralNavigation() {
+  const hasSeenIntro = useSelector((state: State) => state.hasSeenIntro);
+  // const hasSeenIntro = false
+  const dispatch = useDispatch();
+  const {updateHasSeenIntro} = bindActionCreators(actionCreators, dispatch);
+
+  React.useEffect(() => {
+    // Has seen intro, now turn off onboarding
+    updateHasSeenIntro(false);
+    // updateHasSeenIntro(true)
+  });
+
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        {!hasSeenIntro && (
+          <AppStack.Screen name="Onboarding" component={OnboardingDeck} />
+        )}
+        <AppStack.Screen name="RainSlider" component={RainSlider} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
