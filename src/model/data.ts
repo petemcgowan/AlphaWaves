c66a94442d3bf1885eae5d40f923f287a7410a59
Pete McGowan <peteburquette@gmail.com>
2023-11-24 19:49:51 +0400

    interim mods for HLS streaming

---
+++ src/model/data.ts
@@ -0,0 +1,154 @@
+import Sound from 'react-native-sound';
+import {Image} from 'react-native';
+import Config from 'react-native-config';
+
+import videoPosterPath1 from '../../assets/images/posters/rainInPorchOverlookingForest.jpg';
+import videoPosterPath2 from '../../assets/images/posters/RainInACar540x960.jpg';
+import videoPosterPath3 from '../../assets/images/posters/RainInACarAltHD.jpg';
+import videoPosterPath4 from '../../assets/images/posters/HDerGrassyRain.jpg';
+import videoPosterPath5 from '../../assets/images/posters/blueSplashes3Reduced1.jpg';
+import {RainSound} from '../types/RainSound';
+
+const DataTsserverURL = Config.HLS_SERVER_URL;
+console.log('Config.HLS_SERVER_URL:' + Config.HLS_SERVER_URL);
+console.log('DataTsserverURL:' + DataTsserverURL);
+
+const rainSounds: RainSound[] = [
+  {
+    // videoBackground: {
+    //   uri: `${Config.HLS_SERVER_URL}/rainInPorchOverlookingForest/master.m3u8`,
+    // },
+    videoBackground: {
+      uri: `http://localhost:8080/rainInPorchOverlookingForest/master.m3u8`,
+    },
+    videoPoster: require('../../assets/images/posters/rainInPorchOverlookingForest.jpg'),
+    videoPosterUri: Image.resolveAssetSource(videoPosterPath1).uri,
+    timerDialogBackgroundColor: '#136a53',
+    timerDialogFontColor: '#d5e5ea',
+    timerControlsFontColor: '#d5e5ea',
+    playingSound: new Sound(
+      'cozy_cabin_porch_with_heavy_rainstorm_trim.mp3',
+      Sound.MAIN_BUNDLE,
+      error => {
+        if (error) {
+          console.log('failed to load the porch rainstorm sound', error);
+          return;
+        }
+      }
+    ),
+    volume: 0.7,
+    id: '1',
+  },
+  {
+    // videoBackground: {
+    //   uri: `${Config.HLS_SERVER_URL}/RainInACar/master.m3u8`,
+    // },
+    videoBackground: {
+      uri: `http://localhost:8080/RainInACar/master.m3u8`,
+    },
+    // videoBackground: {
+    //   uri: `http://192.168.61.161:8080/RainInACar/master.m3u8`,
+    // },
+    videoPoster: require('../../assets/images/posters/RainInACar540x960.jpg'),
+    videoPosterUri: Image.resolveAssetSource(videoPosterPath2).uri,
+    timerDialogBackgroundColor: '#4d94ca',
+    timerDialogFontColor: '#afb7ad',
+    timerControlsFontColor: '#afb7ad',
+    playingSound: new Sound(
+      'night_rain_on_a_car_trim.mp3',
+      Sound.MAIN_BUNDLE,
+      error => {
+        if (error) {
+          console.log('failed to load the night rain car sound', error);
+          return;
+        }
+      }
+    ),
+    volume: 0.4,
+    id: '2',
+  },
+  {
+    // videoBackground: {
+    //   uri: `${Config.HLS_SERVER_URL}/RainInACarAltHD/master.m3u8`,
+    // },
+    videoBackground: {
+      uri: `http://localhost:8080/RainInACarAltHD/master.m3u8`,
+    },
+    videoPoster: require('../../assets/images/posters/RainInACarAltHD.jpg'),
+    videoPosterUri: Image.resolveAssetSource(videoPosterPath3).uri,
+    timerDialogBackgroundColor: 'rgb(38, 27, 21)',
+    timerDialogFontColor: '#fff',
+    timerControlsFontColor: '#fff',
+    // ['rgb(38, 27, 21)', 'rgb(25, 26, 29)', 'rgb(9, 21, 39)']
+    playingSound: new Sound(
+      'relaxing_sounds_of_light_rain_falling_on_the_car_trim.mp3',
+      // 'relaxing_sounds_of_light_rain_falling_on_the_car_trim2',
+      Sound.MAIN_BUNDLE,
+      error => {
+        if (error) {
+          console.log('failed to load the Rain In A Car AltHD sound', error);
+          return;
+        }
+      }
+    ),
+    volume: 0.7,
+    id: '3',
+  },
+  {
+    videoBackground: {
+      uri: `http://localhost:8080/rainPouringDown/master.m3u8`,
+    },
+    // videoBackground: {
+    //   uri: `${Config.HLS_SERVER_URL}/rainPouringDown/master.m3u8`,
+    // },
+    videoPoster: require('../../assets/images/posters/HDerGrassyRain.jpg'),
+    videoPosterUri: Image.resolveAssetSource(videoPosterPath4).uri,
+    timerDialogBackgroundColor: 'rgb(38, 27, 21)',
+    timerDialogFontColor: '#fff',
+    timerControlsFontColor: '#fff',
+    // ['rgb(38, 27, 21)', 'rgb(25, 26, 29)', 'rgb(9, 21, 39)']
+    playingSound: new Sound(
+      'rain_hitting_a_campervan_roof_window.mp3',
+      // 'relaxing_sounds_of_light_rain_falling_on_the_car_trim2',
+      Sound.MAIN_BUNDLE,
+      error => {
+        if (error) {
+          console.log('failed to load the Rain In A Car AltHD sound', error);
+          return;
+        }
+      }
+    ),
+    id: '4',
+    volume: 0.5,
+  },
+  {
+    // videoBackground: {
+    //   uri: `${Config.HLS_SERVER_URL}/blueSplashes/master.m3u8`,
+    // },
+    videoBackground: {
+      uri: `http://localhost:8080/blueSplashes/master.m3u8`,
+    },
+    // videoBackground: {
+    //   uri: `http://localhost:8080/GentleWavesonaSmallWhiteRockBeach/master.m3u8`,
+    // },
+    videoPoster: require('../../assets/images/posters/blueSplashes3Reduced1.jpg'),
+    videoPosterUri: Image.resolveAssetSource(videoPosterPath5).uri,
+    timerDialogBackgroundColor: 'rgb(9, 21, 39)',
+    timerDialogFontColor: '#e4af91',
+    timerControlsFontColor: '#fff',
+    playingSound: new Sound(
+      'rain_on_water_swimming_pool_snip.mp3',
+      Sound.MAIN_BUNDLE,
+      error => {
+        if (error) {
+          console.log('failed to load the waves rock beach sound', error);
+          return;
+        }
+      }
+    ),
+    volume: 0.4,
+    id: '5',
+  },
+];
+
+export default rainSounds;
---
+++ src/screens/RainSlider.tsx
@@ -0,0 +1,310 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Animated,
+  Platform,
+  TouchableOpacity,
+  NativeModules,
+} from 'react-native';
+import rainSounds from '../model/data';
+// import { useNavigation } from '@react-navigation/native'
+import TimerControls from '../components/TimerControls';
+import CountdownTimer from '../components/CountdownTimer';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Video from 'react-native-video';
+
+const {width, height} = Dimensions.get('window');
+
+const RainSlider = () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [songIndex, setSongIndex] = useState(0);
+  // const [playing, setPlaying] = useState(false)
+
+  const [timerVisible, setTimerVisible] = useState(false);
+
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [soundsPlaying, setSoundsPlaying] = useState([
+    {playing: false},
+    {playing: false},
+    {playing: false},
+    {playing: false},
+    {playing: false},
+  ]);
+  console.log('soundsPlaying' + JSON.stringify(soundsPlaying));
+  const [intentionalVideoPlay, setIntentionalVideoPlay] = useState(true);
+
+  rainSounds[songIndex].playingSound.setVolume(0.9);
+  rainSounds[songIndex].playingSound.setNumberOfLoops(-1);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NativeModules.ExoPlayerModule.preparePlaylist([
+        'cozy_cabin_porch_with_heavy_rainstorm_trim',
+        'night_rain_on_a_car_trim',
+        'relaxing_sounds_of_light_rain_falling_on_the_car_trim',
+        'rain_hitting_a_campervan_roof_window',
+        'rain_on_water_swimming_pool_snip',
+      ]);
+    }
+    scrollX.addListener(({value}) => {
+      const index = Math.round(value / width);
+      if (index === rainSounds.length) {
+        console.log('END OF SLIDES', songIndex);
+      } else {
+        if (index !== songIndex) {
+          if (Platform.OS === 'android') {
+            NativeModules.ExoPlayerModule.switchTrack(index);
+            if (soundsPlaying[songIndex].playing) {
+              updatePlayingStatus(false, songIndex); // set prev song to not play
+              updatePlayingStatus(true, index); // set new song to play
+            }
+          }
+
+          if (Platform.OS === 'ios') {
+            if (rainSounds[songIndex].playingSound._playing) {
+              updatePlayingStatus(false, songIndex);
+              updatePlayingStatus(true, index);
+              // if previous sound if playing, stop it
+              rainSounds[songIndex].playingSound.stop();
+              // play the newly selected sound
+              rainSounds[index].playingSound.play();
+            }
+          }
+          setSongIndex(index);
+        }
+      }
+    });
+
+    return () => {
+      scrollX.removeAllListeners();
+    };
+  }, [scrollX, songIndex, soundsPlaying[songIndex].playing]);
+
+  const updatePlayingStatus = (playingStatus: boolean, index: number) => {
+    const updatedSoundsPlaying = [...soundsPlaying];
+    updatedSoundsPlaying[index].playing = playingStatus;
+    setSoundsPlaying(updatedSoundsPlaying);
+  };
+
+  const togglePlayback = () => {
+    if (Platform.OS === 'android') {
+      NativeModules.ExoPlayerModule.isTrackPlaying((isPlaying: boolean) => {
+        if (isPlaying) {
+          NativeModules.ExoPlayerModule.pauseTrack();
+          updatePlayingStatus(false, songIndex);
+          if (timerVisible) setTimerVisible(false);
+        } else {
+          updatePlayingStatus(true, songIndex);
+          NativeModules.ExoPlayerModule.playTrack();
+        }
+      });
+    }
+
+    if (Platform.OS === 'ios') {
+      if (rainSounds[songIndex].playingSound._playing) {
+        rainSounds[songIndex].playingSound.stop();
+        updatePlayingStatus(false, songIndex);
+
+        if (timerVisible) setTimerVisible(false);
+      } else {
+        updatePlayingStatus(true, songIndex);
+        rainSounds[songIndex].playingSound.play(success => {
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+        });
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Animated.ScrollView
+        horizontal
+        pagingEnabled
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: true}
+        )}
+        scrollEventThrottle={16}
+        showsHorizontalScrollIndicator={false}
+        style={styles.scrollView}>
+        {rainSounds.map((video, index) => (
+          <View style={styles.videoContainer} key={index}>
+            {Platform.OS === 'ios' && (
+              <Video
+                key={video.id}
+                source={video.videoBackground}
+                style={styles.video}
+                muted={true}
+                volume={video.volume}
+                rate={0.6}
+                repeat={true}
+                // buffered={true}
+                // paused={!playing || !intentionalVideoPlay}
+                paused={!soundsPlaying[index].playing || !intentionalVideoPlay}
+                resizeMode="cover"
+              />
+            )}
+            {Platform.OS === 'android' && (
+              <Video
+                key={video.id}
+                source={video.videoBackground}
+                style={styles.video}
+                poster={rainSounds[songIndex].videoPosterUri}
+                posterResizeMode="cover"
+                muted={true}
+                volume={video.volume}
+                rate={0.6}
+                repeat={true}
+                // paused={!playing || !intentionalVideoPlay}
+                paused={!soundsPlaying[index].playing || !intentionalVideoPlay}
+                resizeMode="cover"
+              />
+            )}
+          </View>
+        ))}
+      </Animated.ScrollView>
+      <View style={styles.powerControls}>
+        <TouchableOpacity
+          style={styles.powerIcon}
+          onPress={() => togglePlayback()}>
+          <Ionicons
+            name={'water-outline'}
+            size={250}
+            style={styles.powerIcon}
+            color={
+              soundsPlaying[songIndex].playing
+                ? 'rgba(11, 57, 84, 1)'
+                : 'rgba(191, 215, 234, 0.75)'
+            }
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.timerCountdown}>
+        {timerVisible ? (
+          <View style={styles.timerCountdown}>
+            <CountdownTimer
+              hours={hours}
+              minutes={minutes}
+              seconds={seconds}
+              togglePlayback={togglePlayback}
+              setTimerVisible={setTimerVisible}
+              timerControlsFontColor={
+                rainSounds[songIndex].timerControlsFontColor
+              }
+            />
+          </View>
+        ) : (
+          <View style={styles.timerCountdown}></View>
+        )}
+      </View>
+      <View style={styles.timerControls}>
+        <TimerControls
+          setTimerVisible={setTimerVisible}
+          hours={hours}
+          setHours={setHours}
+          minutes={minutes}
+          setMinutes={setMinutes}
+          seconds={seconds}
+          setSeconds={setSeconds}
+          // playing={playing}
+          playing={soundsPlaying[songIndex].playing}
+          togglePlayback={togglePlayback}
+          intentionalVideoPlay={intentionalVideoPlay}
+          setIntentionalVideoPlay={setIntentionalVideoPlay}
+          timerDialogBackgroundColor={
+            rainSounds[songIndex].timerDialogBackgroundColor
+          }
+          timerDialogFontColor={rainSounds[songIndex].timerDialogFontColor}
+          songIndex={songIndex}
+          rainSounds={rainSounds}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(0, 0, 0)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timerCountdown: {
+    flexBasis: '20%',
+    marginBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    left: 0,
+    bottom: '20%',
+    right: 0,
+  },
+  timerControls: {
+    flexBasis: '18%',
+    marginBottom: 15,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  scrollView: {
+    height: height * 0.7,
+    flexDirection: 'row',
+  },
+  backgroundVideo: {
+    height: height,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    alignItems: 'stretch',
+    bottom: 0,
+    right: 0,
+  },
+  powerControls: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    right: 0,
+    marginRight: width * 0.2,
+    marginLeft: width * 0.2,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    shadowColor: '#ccc',
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 1.34,
+    elevation: 3,
+  },
+  videoContainer: {
+    // width: width - 1,
+    width: width,
+    height: height,
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+  },
+  powerIcon: {
+    opacity: 0.85,
+    paddingTop: 30,
+    height: '100%',
+    borderRadius: 70,
+  },
+});
+
+export default RainSlider;
