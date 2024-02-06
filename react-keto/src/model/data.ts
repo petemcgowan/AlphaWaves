4822cfe1979147e68bc8f5dff94bda651d1e054a
Pete McGowan <peteburquette@gmail.com>
2024-02-06 20:46:49 +0400

    working on a web version of app

--- react-keto/src/model/data.ts
+++ react-keto/src/model/data.ts
@@ -1,32 +1,32 @@
 // Import your sound and image libraries specific to React
-import { Howl } from 'howler'
-import videoPosterPath1 from '../../assets/images/posters/rainInPorchOverlookingForest.jpg'
-import videoPosterPath2 from '../../assets/images/posters/RainInACar540x960.jpg'
-import videoPosterPath3 from '../../assets/images/posters/HeavyRainOnWindowOnTheTrain.jpg'
-import videoPosterPath4 from '../../assets/images/posters/RainPouringDown.jpg'
-import videoPosterPath5 from '../../assets/images/posters/RainPouringDown.jpg'
+import {Howl} from 'howler';
+import videoPosterPath1 from '../../assets/images/posters/rainInPorchOverlookingForest.jpg';
+import videoPosterPath2 from '../../assets/images/posters/RainInACar540x960.jpg';
+import videoPosterPath3 from '../../assets/images/posters/HeavyRainOnWindowOnTheTrain.jpg';
+import videoPosterPath4 from '../../assets/images/posters/RainPouringDown.jpg';
+import videoPosterPath5 from '../../assets/images/posters/RainPouringDown.jpg';
 
-import videoBackground1 from '../../assets/videos/rainInPorchOverlookingForest.mp4'
-import videoBackground2 from '../../assets/videos/RainInACar540x960.mp4'
-import videoBackground3 from '../../assets/videos/HeavyRainOnWindowOnTheTrain.mp4'
-import videoBackground4 from '../../assets/videos/RainPouringDown.mp4'
-import videoBackground5 from '../../assets/videos/GentleWavesonaSmallWhiteRockBeach.mp4'
+import videoBackground1 from '../../assets/videos/rainInPorchOverlookingForest.mp4';
+import videoBackground2 from '../../assets/videos/RainInACar540x960.mp4';
+import videoBackground3 from '../../assets/videos/HeavyRainOnWindowOnTheTrain.mp4';
+import videoBackground4 from '../../assets/videos/RainPouringDown.mp4';
+import videoBackground5 from '../../assets/videos/GentleWavesonaSmallWhiteRockBeach.mp4';
 
-import videoPoster1 from '../../assets/images/posters/rainInPorchOverlookingForest.jpg'
-import videoPoster2 from '../../assets/images/posters/RainInACar540x960.jpg'
-import videoPoster3 from '../../assets/images/posters/HeavyRainOnWindowOnTheTrain.jpg'
-import videoPoster4 from '../../assets/images/posters/RainPouringDown.jpg'
-import videoPoster5 from '../../assets/images/posters/GentleWavesonaSmallWhiteRockBeach.jpg'
+import videoPoster1 from '../../assets/images/posters/rainInPorchOverlookingForest.jpg';
+import videoPoster2 from '../../assets/images/posters/RainInACar540x960.jpg';
+import videoPoster3 from '../../assets/images/posters/HeavyRainOnWindowOnTheTrain.jpg';
+import videoPoster4 from '../../assets/images/posters/RainPouringDown.jpg';
+import videoPoster5 from '../../assets/images/posters/GentleWavesonaSmallWhiteRockBeach.jpg';
 
 interface RainSound {
-  videoBackground: any
-  videoPoster: any
-  videoPosterUri: string
-  timerDialogBackgroundColor: string
-  timerDialogFontColor: string
-  timerControlsFontColor: string
-  playingSound: Howl
-  id: string
+  videoBackground: any;
+  videoPoster: any;
+  videoPosterUri: string;
+  timerDialogBackgroundColor: string;
+  timerDialogFontColor: string;
+  timerControlsFontColor: string;
+  playingSound: Howl;
+  id: string;
 }
 
 const rainSounds: RainSound[] = [
@@ -60,7 +60,7 @@ const rainSounds: RainSound[] = [
       html5: true,
       autoplay: false,
       loop: true,
-      volume: 0.9,
+      volume: 0.6,
     }),
     id: '2',
   },
@@ -108,10 +108,10 @@ const rainSounds: RainSound[] = [
       html5: true,
       autoplay: false,
       loop: true,
-      volume: 0.9,
+      volume: 0.7,
     }),
     id: '5',
   },
-]
+];
 
-export default rainSounds
+export default rainSounds;
