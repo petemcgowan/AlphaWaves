0d0a03daabe97039ad27fa811cf2e575e66ec730
Pete McGowan <peteburquette@gmail.com>
2024-03-19 19:20:43 +0400

    movein greact-keto to own project

--- react-keto/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './App.css'
-import CentralNavigation from './components/CentralNavigation'
-
-function App() {
-  return <CentralNavigation />
-}
-
-export default App
