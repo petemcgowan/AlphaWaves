c6525f337fe26a45f2cfa3a6242a38bd9d66fe7a
Peter McGowan <peteburquette@gmail.com>
2026-01-02 01:37:46 +0000

    mocks and unit tests

--- src/screens/__mocks__/redux-persist.js
+++
@@ -1,9 +0,0 @@
-const reduxPersist = {
-  persistReducer: jest.fn().mockImplementation((config, reducers) => reducers),
-  persistStore: jest.fn().mockImplementation(() => ({
-    getState: jest.fn(),
-    dispatch: jest.fn(),
-  })),
-};
-
-module.exports = reduxPersist;
